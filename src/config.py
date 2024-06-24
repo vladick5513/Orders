147d502c18f6b212bda5215cc78ee10732ad49df
vladick5513 <2006krickunow@gmail.com>
2024-06-24 20:59:23 +0300

    moved the env file from the venv directory

--- src/config.py
+++ src/config.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
         # postgresql+psycopg://postgres:postgres@localhost:5432/sa
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file="../venv/.env")
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
 
