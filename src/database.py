d5ef47713af51a788eec0bda8c156b691e988c97
vladick5513 <2006krickunow@gmail.com>
2024-08-26 11:56:18 +0300

    fixed the import path from the settings

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker, declarative_base
 from sqlalchemy import URL, create_engine, text, String
-from config import settings
+from src.config import settings
 
 Base = declarative_base()
 
