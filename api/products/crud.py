d6e2549423072ea362c059386333cd8f477a0bec
vladick5513 <2006krickunow@gmail.com>
2024-08-28 16:45:42 +0300

    create products CRUDs

---
+++ api/products/crud.py
@@ -0,0 +1,37 @@
+from api.products.schemas import ProductCreate, ProductUpdate
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from src.models import Product
+
+
+def read_product(db:Session, product_id: int):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+def create_product(db: Session, product: ProductCreate):
+    db_product = Product(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+
+def update_product(db: Session, product_id: int, product: ProductUpdate):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    for key, value in product.dict(exclude_unset=True).items():
+        setattr(db_product, key, value)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def delete_product(db: Session, product_id: int):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    db.delete(db_product)
+    db.commit()
+    return {"ok": True}
