0e4ed59923b4003e5608ba85595d8b83a9fafe1a
vladick5513 <2006krickunow@gmail.com>
2024-08-28 16:40:40 +0300

    create customers views

---
+++ api/customers/views.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from api.customers.crud import create_customer, read_customer, update_customer, delete_customer
+from src.database import get_db
+from api.customers.schemas import CustomerCreate, CustomerUpdate, CustomerResponse
+
+router = APIRouter(prefix="/customers", tags=["Customers"])
+
+
+@router.post("/", response_model=int)
+def create_customer_endpoint(customer: CustomerCreate, db: Session = Depends(get_db)):
+    return create_customer(db, customer)
+
+# Чтение
+@router.get("/{customer_id}", response_model=CustomerResponse)
+def read_customer_endpoint(customer_id: int, db: Session = Depends(get_db)):
+    return read_customer(db, customer_id)
+
+# Обновление
+@router.put("/{customer_id}")
+def update_customer_endpoint(customer_id: int, customer: CustomerUpdate, db: Session = Depends(get_db)):
+    return update_customer(db, customer_id, customer)
+
+# Удаление
+@router.delete("/{customer_id}")
+def delete_customer_endpoint(customer_id: int, db: Session = Depends(get_db)):
+    return delete_customer(db, customer_id)
