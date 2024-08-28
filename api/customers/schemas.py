80b17691e3e01146aed0e619642c481b87a1942e
vladick5513 <2006krickunow@gmail.com>
2024-08-28 16:40:06 +0300

    create customers schemas

---
+++ api/customers/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class CustomerCreate(BaseModel):
+    name: str
+    email: str
+    phone: str
+    address: str
+class CustomerUpdate(BaseModel):
+    name: str = None
+    email: str = None
+    phone: str = None
+    address: str = None
+
+class CustomerResponse(BaseModel):
+    id: int
+    name: str
+    email: str
