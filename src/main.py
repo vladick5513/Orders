d67e228352b6d05acd19a99f4af5a6aff84d4d09
vladick5513 <2006krickunow@gmail.com>
2024-08-29 12:07:35 +0300

    add shipping router

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from api.customers.views import router as customers_router
 from api.orders.views import router as orders_router
 from api.order_items.views import router as order_items_router
 from api.payments.views import router as payment_router
+from api.shipping.views import router as shipping_router
 
 app = FastAPI()
 app.include_router(products_router)
@@ -13,6 +14,7 @@ app.include_router(customers_router)
 app.include_router(orders_router)
 app.include_router(order_items_router)
 app.include_router(payment_router)
+app.include_router(shipping_router)
 
 
 
