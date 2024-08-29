875ba1242105f51028f54a42d754a47100829035
vladick5513 <2006krickunow@gmail.com>
2024-08-29 11:26:24 +0300

    add order items router

--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ from fastapi import FastAPI
 from api.products.views import router as products_router
 from api.customers.views import router as customers_router
 from api.orders.views import router as orders_router
+from api.order_items.views import router as order_items_router
 
 app = FastAPI()
 app.include_router(products_router)
 app.include_router(customers_router)
 app.include_router(orders_router)
+app.include_router(order_items_router)
 
 
 
