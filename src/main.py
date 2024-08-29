5a895edc4597418a1681e96d12bcbe6594e9e26d
vladick5513 <2006krickunow@gmail.com>
2024-08-29 12:06:28 +0300

    add payment router

--- src/main.py
+++ src/main.py
@@ -5,12 +5,14 @@ from api.products.views import router as products_router
 from api.customers.views import router as customers_router
 from api.orders.views import router as orders_router
 from api.order_items.views import router as order_items_router
+from api.payments.views import router as payment_router
 
 app = FastAPI()
 app.include_router(products_router)
 app.include_router(customers_router)
 app.include_router(orders_router)
 app.include_router(order_items_router)
+app.include_router(payment_router)
 
 
 
