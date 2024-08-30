0cac0b1d1efe3eb9fe8b9ce8ffbc3815af18c05c
vladick5513 <2006krickunow@gmail.com>
2024-08-30 14:08:04 +0300

    add payments CRUDs

---
+++ api/payments/crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from api.payments.schemas import PaymentCreate, PaymentUpdate
+from src.models import Payment
+
+def create_payment(db: Session, payment: PaymentCreate):
+    db_payment = Payment(**payment.dict())
+    db.add(db_payment)
+    db.commit()
+    db.refresh(db_payment)
+    return db_payment.id
+
+def read_payment(db: Session, payment_id: int):
+    return db.query(Payment).filter(Payment.id == payment_id).first()
+
+def update_payment(db: Session, payment_id: int, payment: PaymentUpdate):
+    db_payment = db.query(Payment).filter(Payment.id == payment_id).first()
+    if not db_payment:
+        return None
+    for key, value in payment.dict(exclude_unset=True).items():
+        setattr(db_payment, key, value)
+    db.commit()
+    db.refresh(db_payment)
+    return db_payment
+
+def delete_payment(db: Session, payment_id: int):
+    db_payment = db.query(Payment).filter(Payment.id == payment_id).first()
+    if db_payment:
+        db.delete(db_payment)
+        db.commit()
+        return True
+    return False
