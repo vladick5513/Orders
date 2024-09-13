53b456da623add4fead09ee2d011a1f5ec7031e8
vladick5513 <2006krickunow@gmail.com>
2024-09-13 08:58:00 +0300

    create User and Access Token model

--- src/models.py
+++ src/models.py
@@ -1,11 +1,31 @@
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, func, Text, Float, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import Base
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from src.types.user_id import UserIdType
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTable
 
-class User(SQLAlchemyBaseUserTable[int], Base):
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+class User(Base, SQLAlchemyBaseUserTable[UserIdType]):
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType]):
+
+    user_id: Mapped[UserIdType] = mapped_column(Integer, ForeignKey("users.id", ondelete="cascade"), nullable=False)
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, AccessToken)
 
 
 class Customers(Base):
