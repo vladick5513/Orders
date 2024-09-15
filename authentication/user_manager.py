15acab74f8d459e956ec7433c7b36713ca71ce5e
vladick5513 <2006krickunow@gmail.com>
2024-09-15 16:05:53 +0300

    user manager handle request verify

--- authentication/user_manager.py
+++ authentication/user_manager.py
@@ -17,14 +17,11 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         log.warning("User %r has registered.", user.id)
 
-    async def on_after_forgot_password(
-        self, user: User, token: str, request: Optional[Request] = None
-    ):
-        log.warning("User %r has forgot their password. Reset token: %r", user.id, token)
-
     async def on_after_request_verify(
         self, user: User, token: str, request: Optional[Request] = None
     ):
         log.warning("Verification requested for user %r. Verification token: %r", user.id, token)
+
+
 
 
