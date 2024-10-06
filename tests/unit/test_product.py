9b1277960ce80e04a6fcff218ca9cca8671a22f8
vladick5513 <2006krickunow@gmail.com>
2024-10-07 01:01:36 +0300

    fix the def in test_product, add fixture

--- tests/unit/test_product.py
+++ tests/unit/test_product.py
@@ -1,50 +1,32 @@
 import pytest
 from api.products.crud import create_product, read_product, update_product, delete_product
 from api.products.schemas import ProductCreate, ProductUpdate
-from src.config import settings
 from fastapi import HTTPException
 
 
 
-async def test_create_product(async_session, setup_db):
-    product_data = ProductCreate(
-        name="Test Product",
-        description="This is a test product",
-        price=1000,
-        stock_quantity=10
-    )
-    product = await create_product(async_session, product_data)
+
+async def test_create_product(async_session, setup_product):
+    product = setup_product
     assert product is not None
     assert product.name == "Test Product"
     assert product.price == 1000
     assert product.stock_quantity == 10
 
 
-async def test_read_product(async_session):
-    product_data = ProductCreate(
-        name="Test Product",
-        description="This is a test product",
-        price=1000,
-        stock_quantity=10
-    )
-    product = await create_product(async_session, product_data)
+async def test_read_product(async_session, setup_product):
+    product = setup_product
     fetched_product = await read_product(async_session, product.id)
     assert fetched_product is not None
     assert fetched_product.id == product.id
     assert fetched_product.name == "Test Product"
 
 
-async def test_update_product(async_session):
-    product_data = ProductCreate(
-        name="Old Product",
-        description="This is an old product",
-        price=500,
-        stock_quantity=5
-    )
-    product = await create_product(async_session, product_data)
+async def test_update_product(async_session, setup_product):
+    product = setup_product
     updated_data = ProductUpdate(
         name="Updated Product",
-        description="This is an new product",
+        description="This is a new product",
         price=1500,
         stock_quantity=20
     )
@@ -55,14 +37,8 @@ async def test_update_product(async_session):
     assert updated_product.stock_quantity == 20
 
 
-async def test_delete_product(async_session):
-    product_data = ProductCreate(
-        name="Test Product",
-        description="This is a test product",
-        price=1000,
-        stock_quantity=10
-    )
-    product = await create_product(async_session, product_data)
+async def test_delete_product(async_session, setup_product):
+    product = setup_product
     await delete_product(async_session, product.id)
 
     with pytest.raises(HTTPException) as exc_info:
