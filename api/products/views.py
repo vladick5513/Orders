b66db7d542ef14765e3e5d192b08b05d0521fd27
vladick5513 <2006krickunow@gmail.com>
2024-11-08 17:53:10 +0300

    fix router get

--- api/products/views.py
+++ api/products/views.py
@@ -15,7 +15,7 @@ async def create_product_endpoint(product: ProductCreate,db: AsyncSession = Depe
 async def read_product_endpoint(product_id: int, db: AsyncSession = Depends(get_db)):
     return await read_product(db, product_id)
 
-@router.get("/all", response_model=list[ProductResponse])
+@router.get("/", response_model=list[ProductResponse])
 async def read_all_products_endpoint(db: AsyncSession = Depends(get_db)):
     return await read_all_products(db)
 
@@ -25,7 +25,7 @@ async def read_products_by_price_range_endpoint(min_price: int, max_price: int, 
 
 @router.put("/{product_id}", response_model=ProductResponse)
 async def update_product_endpoint(product_id: int, product:ProductUpdate, db: AsyncSession = Depends(get_db)):
-    return await update_product(db, product, product_id)
+    return await update_product(db, product_id, product)
 
 @router.delete("/{product_id}", response_model=ProductResponse)
 async def delete_product_endpoint(product_id: int, db: AsyncSession = Depends(get_db)):
